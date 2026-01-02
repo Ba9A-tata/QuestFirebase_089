73013bd8b7bf6904a1a23f274037f983db6dbf0b
Ba9A-tata <yogadzakkii@gmail.com>
2026-01-02 18:19:49 +0700

    menambahkan kode untuk file DestinasiHome.kt

---
+++ app/src/main/java/com/example/firebase_yoga089/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebase_yoga089.view.route
+
+import com.example.firebase_yoga089.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
