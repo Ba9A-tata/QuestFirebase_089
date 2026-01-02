7496e7dcd61ac49cb9cdaefe49a0648ce9f9fe87
Ba9A-tata <yogadzakkii@gmail.com>
2026-01-02 18:22:17 +0700

    menambahkan kode untuk file DestinasiDetail.kt

---
+++ app/src/main/java/com/example/firebase_yoga089/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.firebase_yoga089.view.route
+
+import com.example.firebase_yoga089.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
