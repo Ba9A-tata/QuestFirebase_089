6a5b7bfdc639dc4c0616f836304efd859114dc4c
Ba9A-tata <yogadzakkii@gmail.com>
2026-01-08 11:19:57 +0700

    menambahkan fun detailadatasiswa

--- app/src/main/java/com/example/firebase_yoga089/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase_yoga089/view/HalamanDetail.kt
@@ -97,4 +97,37 @@ private fun BodyDetailDataSiswa(
         modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen
             .padding_medium))
-    ) 
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        when(statusUIDetail){
+            is StatusUIDetail.Success -> DetailDataSiswa(
+                siswa = statusUIDetail.satusiswa,
+                modifier = Modifier.fillMaxWidth()
+            )
+            else -> {}
+        }
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailDataSiswa(
+    siswa: Siswa?, modifier: Modifier = Modifier
+) {
+    
