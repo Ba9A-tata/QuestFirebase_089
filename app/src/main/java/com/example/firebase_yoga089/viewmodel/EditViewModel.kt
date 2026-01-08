8e7b08caa3d439197baeb819a5b520c07378daaa
Ba9A-tata <yogadzakkii@gmail.com>
2026-01-08 11:34:29 +0700

    menambahkan private fun validasiinput

--- app/src/main/java/com/example/firebase_yoga089/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase_yoga089/viewmodel/EditViewModel.kt
@@ -34,5 +34,10 @@ RepositorySiswa
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
                 (detailSiswa))
     }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
 }
